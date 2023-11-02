c30736de0189062bf4cbf5837034b5d35c71c642
Mosihuddin <sheikhkinu@gmail.com>
2023-11-02 22:11:58 +0600

    last

--- countries.js
+++ countries.js
@@ -17,26 +17,69 @@ function displaycountry(countries){
    
     div.classList.add('country');
     
+    div.innerHTML =`
+     
+    <h3> Country Name: ${country.name.official}
+</h3>   
+<p> Capital : ${country.capital}</p>
+<button onclick = "loadCountriesByName('${country.name.official}')"> Details </button>
+    `;
+    countriesDiv.appendChild(div);
     
-        
-        const h3 = document.createElement('h3');
+        // my old code :)
+    //     const h3 = document.createElement('h3');
     
-    h3.innerText =`Country Name : ${country.name.official}`;
-        div.appendChild(h3);
+    // h3.innerText =`Country Name : ${country.name.official}`;
+    //     div.appendChild(h3);
 
-        const p = document.createElement('p');
-        p.innerText= `Capital: ${country.capital}`;
-        div.appendChild(p);
-        countriesDiv.appendChild(div);
+    //     const p = document.createElement('p');
+    //     p.innerText= `Capital: ${country.capital}`;
+       
+    //     div.appendChild(p);
+        
         
 
-        // const button = document.createElement('button');
-        // // button.addEventListener= onclick();
-        // button.innerHTML  = `details`;
-        // div.appendChild(button);
-        // countriesDiv.appendChild(div);
+    //     // const button = document.createElement('button');
+    //     // // button.addEventListener= onclick();
+    //     // button.innerHTML  = 'details';
+    //     // div.appendChild(button);
+    //     // countriesDiv.appendChild(div);
+    //     // div.innerHTML=
+    //     // `<button></button>`
+
+    //     const button = document.createElement('button');
+    //     button.innerHTML='details';
+    //     div.appendChild(button);
+    //     countriesDiv.appendChild(div);
        
     
 
     }
 }
+
+function loadCountriesByName(name){
+    
+    const url =` https://restcountries.com/v3.1/name/${name}`
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> DisplayCountryDetail(data[0]));
+}
+
+function DisplayCountryDetail(country){
+    console.log(country);
+  const countrydiv = document.getElementById('country-detail');
+  countrydiv.innerHTML= `
+    <h4>${country.name.official}</h4>
+    <p>${country.capital}</p>
+    <img src ="${country.flags}">
+  
+                  `
+}
+
+
+// function displayDetails(){
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data=>displaycountry(data))
+// }
